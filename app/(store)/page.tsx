a453f009ad7c601c202cd675a8ea63d5ad4655aa
Shaurya Jha <jhashaurya222@gmail.com>
2025-05-30 23:20:41 +0530

    Finished Header component with complete responsive behavior and sign in implementation except for passkeys implementation.

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World! 123</h1>
+      <h1>Hello World</h1>
       <Button>Click me!</Button>
     </div>
   );
