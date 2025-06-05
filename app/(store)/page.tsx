ba5617bc384059434e8e1c8781126c301e55d6e1
Shaurya Jha <jhashaurya222@gmail.com>
2025-06-06 00:09:51 +0530

    Added Black friday sale in the sales schema and logic for fetching and rendering it.

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,3 +1,4 @@
+import BlackFridayBanner from "@/components/BlackFridayBanner";
 import ProductsView from "@/components/ProductsView";
 import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
@@ -7,7 +8,7 @@ export default async function Home() {
   const categories = await getAllCategories();
   return (
     <div>
-      <h1>Hello World</h1>
+      <BlackFridayBanner />
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductsView products={products} categories={categories} />
       </div>
