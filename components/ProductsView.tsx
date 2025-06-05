94874c31a8f812a25ece5a63c0b5a1e083add8d0
Shaurya Jha <jhashaurya222@gmail.com>
2025-06-05 12:14:13 +0530

    Successfully setup products fetching logic from the backend along with styling to some initial ones.

---
+++ components/ProductsView.tsx
@@ -0,0 +1,26 @@
+import { Category, ProductType } from "@/sanity.types";
+import ProductGrid from "./ProductGrid";
+
+interface ProductsViewProps {
+  products: ProductType[];
+  categories: Category[];
+}
+
+export default function ProductsView({
+  products,
+  categories,
+}: ProductsViewProps) {
+  return (
+    <section className="flex flex-col">
+      <div className="w-full sm:w-[200px]">
+        {/* <CategorySelectorComponent categories={categories} /> */}
+      </div>
+      <div className="flex-1">
+        <div>
+          <ProductGrid products={products} />
+          <hr className="w-1/2 sm:w-3/4" />
+        </div>
+      </div>
+    </section>
+  );
+}
