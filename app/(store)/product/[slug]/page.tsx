6d78ffd1c9320978080a0b97219065dd431ed56c
Shaurya Jha <jhashaurya222@gmail.com>
2025-06-09 22:49:50 +0530

    Created an app-wide store with the help of zustand along with actions, implemented functionality for changing products and their quantity in basket from their personal pages.

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AddToBasketButton from "@/components/AddToBasketButton";
 import { imageUrl } from "@/lib/imageUrl";
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { PortableText } from "next-sanity";
@@ -49,6 +50,9 @@ export default async function ProductPage({
                 <PortableText value={product.description} />
               )}
             </div>
+          </div>
+          <div className="mt-6">
+            <AddToBasketButton product={product} isDisabled={isOutOfStock} />
           </div>
         </div>
       </div>
